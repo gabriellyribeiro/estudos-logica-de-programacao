a83f20f70a6dc3c6f5c35d9acfa7e6331ffbf8f9
Gabrielly Ribeiro <gabrielly.c.ribeiro@gmail.com>
2019-11-24 17:54:44 -0300

    Criei estrutura de dados para imprimir um vetor com numeros impares

--- Lista1/java/Item3lista.java
+++ Lista1/java/Item3lista.java
@@ -1,24 +1,30 @@
 package item3lista;
+
 import java.util.Scanner;
+
 public class Item3lista {
-  // 3.  Dado um número inteiro positivo n, imprimir os n primeiros naturais ímpares. 
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-          System.out.println("Escolha a quantidade de elementos ímpares: ");
-            int valor = entrada.nextInt();
-            int vetor[] = new int[valor];
-            int j=0, numero_impar=0;
-            
-            for(int i=0;i<valor;){   
-                numero_impar++;
-                if (numero_impar % 3 ==0)
-                                 {
-                    i++;
-                    System.out.println("Ímpares: "+numero_impar);
-                }
-                
-                
-                                    }
+  // 3.  Dado um número inteiro positivo n, imprimir os n primeiros naturais
+  // ímpares.
+  public static void main(String[] args) {
+    // Pegando a entrada
+    Scanner sn = new Scanner(System.in);
+    System.out.println("Escolha a quantidade de elementos ímpares: ");
+    int entrada = sn.nextInt();
+
+    // Gerando um vetor para contagem
+    int tamanho = entrada * 2;
+    int[] contagem = new int[tamanho];
+    for (int i = 0; i < tamanho; i++) {
+      contagem[i] = i + 1;
+    }
+
+    // Encontrando impares dentro do array de contagem
+    int[] impares = new int[entrada];
+    for (int i = 0; i < contagem.length; i++) {
+      if (contagem[i] % 2 == 1) {
+        System.out.println(contagem[i]);
+      }
+    }
+
   }
-  }
-    
+}
