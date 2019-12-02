f9b498502ec47c6933a4b46e1ca0e8b3edd38772
Gabrielly Ribeiro <gabrielly.c.ribeiro@gmail.com>
2019-12-02 10:07:53 -0300

    Aula Js da Generation

---
+++ generation/js/Gaby/js/script.js
@@ -0,0 +1,17 @@
+var amigo = {
+        //Atributos
+        nome: "Gabrielly",
+        sobrenome: "Ribeiro",
+        idade: 24,
+        altura: 1.58,
+        nascimento: 1995-03-17,
+        //Métodos
+        aprender: function(){
+                console.log("Cursos de T.I");
+                
+        }
+}
+
+console.log(amigo["nome"] + " " + amigo.altura)
+
+amigo.aprender()
